7cabd5a84f8aead83ddafbd13908aa2e15e4de12
Credoecspa <ezejioforchiemerie708@gmail.com>
2022-12-09 15:41:14 -0800

    kilo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,17 +4,17 @@
 /**
  *  * Description: main - A program that prints the alphabet in lowercase.
  *   * You can only use the putchar function
- *    * Return: 0
+ *   * Return: 0
  */
 
 int main(void)
 {
 	int n = 'a';
 	while (n <= 'z')
-	{
-		putchar(n);
-		n += 1;
-	}
+{
+	putchar(n);
+	n += 1;
+}
 	putchar('\n');
 	return (0);
 }
