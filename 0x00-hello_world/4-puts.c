adb63730c9763c821e4a36ef924163cc8b0c4c8a
Credoecspa <ezejioforchiemerie708@gmail.com>
2022-12-08 17:22:15 -0800

    hhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *  * Description: main - Puts the texts to stdout.
- *   * Return: 0 should code runs successfully.
- *    */
+ *   * Return: 0 should code runs successfully.   
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
