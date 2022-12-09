014be94f97034c498716eb724b762f81995c7af1
Credoecspa <ezejioforchiemerie708@gmail.com>
2022-12-08 17:33:18 -0800

    popi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  *  * Description: main - Prints out texts to stdout.
  *   * Return: 0 should the code runs successfully.
- *    */
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
