b02775b3ed7ec6f370e2e39dbe4e56cd6bd0d7ba
Credoecspa <ezejioforchiemerie708@gmail.com>
2022-12-13 17:44:36 -0800

    npa

--- 0x02-functions_nested_loops/7+print_last_digit.c
+++ 0x02-functions_nested_loops/7+print_last_digit.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * print_last_digit - Prints the last digit of a number.
@@ -8,8 +8,11 @@ nclude "main.h"
 int print_last_digit(int n)
 {
 int last_digit = n % 10;
+
 if (last_digit < 0)
 last_digit *= -1;
+
 _putchar(last_digit + '0');
+
 return (last_digit);
 }
